782c2821caea9591575074596355423ab01db3c2
naathaliie <nathalielarsson@live.se>
2024-08-21 16:08:55 +0200

    som basic and start on router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./main.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./routes/HomePage/HomePage";
+import OrderPage from "./routes/OrderPage/OrderPage";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/orders",
+    element: <OrderPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/OrderPage/OrderPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./OrderPage.scss";
+
+const OrderPage = () => {
+  return (
+    <div className="OrderPage">
+      <h1>OrderPage</h1>
+    </div>
+  );
+};
+
+export default OrderPage;
